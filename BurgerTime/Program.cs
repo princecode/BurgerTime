c91606a7bc1a077f4409a5def52a192afa684200
Prince <princemwilliams@gmail.com>
2016-06-17 02:18:44 -0400

    First desparate attempt at first order

--- BurgerTime/Program.cs
+++ BurgerTime/Program.cs
@@ -10,6 +10,31 @@ namespace BurgerTime
     {
         static void Main(string[] args)
         {
+
+            // Order a burger
+
+            //This creates the Welcome and menu
+
+            Console.WriteLine("Welcome to BurgerTime!");
+            /* Console.WriteLine("Would you like a burger or a BURGER?!"); 
+
+             //string burger = Console.ReadLine();
+            public static bool BurgerOrder(string bgr)
+        {
+            return bgr.Contains("burger");
+
+        }
+
+            // "Want Fries?" Y/N?           
+            // If Y: order fries (maybe include actual options)
+            //If N "want drink?" Y/N?        
+            // pay cashier
+            
+
+
+        Console.ReadKey();
+
+
         }
     }
 }
