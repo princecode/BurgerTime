9677a398cf865f9da6230a42ca5e12d788acf539
Prince <princemwilliams@gmail.com>
2016-06-17 02:38:46 -0400

    First steps of Burger Order system

--- BurgerTime/Program.cs
+++ BurgerTime/Program.cs
@@ -15,24 +15,32 @@ namespace BurgerTime
 
             //This creates the Welcome and menu
 
-            Console.WriteLine("Welcome to BurgerTime!");
-            /* Console.WriteLine("Would you like a burger or a BURGER?!"); 
+            Console.WriteLine("Welcome to BurgerTime! Please Press <ENTER> to start your order.");
 
-             //string burger = Console.ReadLine();
-            public static bool BurgerOrder(string bgr)
-        {
-            return bgr.Contains("burger");
+            Console.ReadKey();
 
-        }
+            Console.WriteLine("Would you like a burger or a BURGER?!"); 
+
+             string burgOrd = Console.ReadLine();
+
+            Console.WriteLine("You selceted a" burgOrd. " Please press <ENTER> to continue");
+
+            //return burger.Contains(burger);  <---- Might be able to use to calc order
+
+
+
+
+
 
             // "Want Fries?" Y/N?           
             // If Y: order fries (maybe include actual options)
             //If N "want drink?" Y/N?        
             // pay cashier
-            
+            //return burger.Contains(burger);  <---- Might be able to use to print order and cost
 
 
-        Console.ReadKey();
+
+            Console.ReadKey();
 
 
         }
